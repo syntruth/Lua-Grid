63e95279bdee4e910aceb0bd1f25dccd5d312480
Syntruth <syntruth@gmail.com>
2011-06-15 10:52:20 -0400

    Cleaned up grid.lua file.

--- grid.lua
+++ grid.lua
@@ -75,377 +75,392 @@ grid = {}
 -- Some 'constant' values.
 -- These are also exposed to the global scope.
 --]]
-GRID_OUTSIDE = "GRID_OUTSIDE"
+GRID_OUTSIDE   = "GRID_OUTSIDE"
 GRID_NOT_VALID = "GRID_NOT_VALID"
 GRID_NIL_VALUE = "GRID_NIL_VALUE"
 
 --[[ Traversal Vector Constants ]]
-GRID_TOP_LEFT = 1
-GRID_TOP = 2
-GRID_TOP_RIGHT = 3
-GRID_LEFT = 4
-GRID_CENTER = 5
-GRID_RIGHT = 6
-GRID_BOTTOM_LEFT = 7
-GRID_BOTTOM = 8
+GRID_TOP_LEFT     = 1
+GRID_TOP          = 2
+GRID_TOP_RIGHT    = 3
+GRID_LEFT         = 4
+GRID_CENTER       = 5
+GRID_RIGHT        = 6
+GRID_BOTTOM_LEFT  = 7
+GRID_BOTTOM       = 8
 GRID_BOTTOM_RIGHT = 9
 
 
 --[[ Grid Class Definition ]]
 function grid.Grid(sizex, sizey, def_value)
-	local g = {}
+  local g = {}
 
-	--[[ Default Grid size is 4x4. ]]
-	if type(sizex) ~= "number" or sizex == nil then
-		sizex = 4
-	end
+  --[[ Default Grid size is 4x4. ]]
+  if type(sizex) ~= "number" or sizex == nil then
+    sizex = 4
+  end
 
-	if type(sizey) ~= "number" or sizey == nil then
-		sizey = 4
-	end
+  if type(sizey) ~= "number" or sizey == nil then
+    sizey = 4
+  end
 
-	g.size_x = sizex
-	g.size_y = sizey
+  g.size_x = sizex
+  g.size_y = sizey
 
-	if def_value == nil then
-		g.def_value = GRID_NIL_VALUE
-	else
-		g.def_value = def_value
-	end
+  if def_value == nil then
+    g.def_value = GRID_NIL_VALUE
+  else
+    g.def_value = def_value
+  end
 
-	g._grid = {}
+  --[[ Internal grid table. ]]
+  g._grid = {}
 
+  --[[ Build the grid and insert the def values. ]]
+  for x=1, sizex do
+    g._grid[x] = {}
+    for y=1, sizey do
+      table.insert(g._grid[x], def_value)
+    end
+  end
 
-	--[[ Build the grid and insert the def values. ]]
-	for x=1, sizex do
-		g._grid[x] = {}
-		for y=1, sizey do
-			table.insert(g._grid[x], def_value)
-		end
-	end
+  --[[ METHOD DEFINITIONS ]]
 
-	--[[ METHOD DEFINITIONS ]]
+  --[[
+  -- This checks to see if a given x,y pair are within
+  -- the boundries of the grid.
+  --]]
+  function g:is_valid(x, y)
 
-	--[[
-	-- This checks to see if a given x,y pair are within
-	-- the boundries of the grid.
-	--]]
-	function g:is_valid(x, y)
+    if (x == nil or type(x) ~= "number") or 
+      (y == nil or type(y) ~= "number") then
+      return false
+    end
 
-		if (x == nil or type(x) ~= "number") or 
-			(y == nil or type(y) ~= "number") then
-			return false
-		end
+    if (x > 0 and x <= self.size_x) and (y > 0 and y <= self.size_y) then
+      return true
+    else
+      return false
+    end
+  end
 
-		if (x > 0 and x <= self.size_x) and (y > 0 and y <= self.size_y) then
-			return true
-		else
-			return false
-		end
+  --[[ Gets the data in a given x,y cell. ]]
+  function g:get_cell(x, y)
+    if self:is_valid(x, y) then return self._grid[x][y] end
+  end
 
-	end
+  --[[
+  -- This method will return a set of cell data in a table.
+  -- The 'cells' argument should be a table of x,y pairs of
+  -- the cells being requested.
+  --]]
+  function g:get_cells(cells)
+    data = {}
 
-	--[[ Gets the data in a given x,y cell. ]]
-	function g:get_cell(x, y)
-		if self:is_valid(x, y) then
-			return self._grid[x][y]
-		end
-	end
+    local x, y, obj
 
-	--[[
-	-- This method will return a set of cell data in a table.
-	-- The 'cells' argument should be a table of x,y pairs of
-	-- the cells being requested.
-	--]]
-	function g:get_cells(cells)
-		data = {}
-		local x, y, obj
-		if type(cells) ~= "table" then
-			return data
-		end
+    if type(cells) ~= "table" then
+      return data
+    end
 
-		for _, v in cells do
-			x, y = unpack(v)
-			if self:is_valid(x, y) then
-				obj = self._grid[x][y]
-				table.insert(data, obj)
-			end
-		end
+    for _, v in cells do
+      x, y = unpack(v)
 
-		return data
-	end
+      if self:is_valid(x, y) then
+        table.insert(data, self._grid[x][y])
+      end
+    end
 
-	--[[ Sets a given x,y cell to the data object. ]]
-	function g:set_cell(x, y, obj)
-		if self:is_valid(x, y) then
-			self._grid[x][y] = obj
-		end
-	end
+    return data
+  end
 
-	--[[ Resets a given x,y cell to the grid default value. ]]
-	function g:reset_cell(x, y)
-		if self:is_valid(x, y) then
-			self._grid[x][y] = self.def_value
-			return true
-		else
-			return false
-		end
-	end
+  --[[ Sets a given x,y cell to the data object. ]]
+  function g:set_cell(x, y, obj)
+    if self:is_valid(x, y) then
+      self._grid[x][y] = obj
+    end
+  end
 
-	--[[ Resets the entire grid to the default value. ]]
-	function g:reset_all()
-		for x=1, self.size_x do
-			for y=1, self.size_y do
-				self._grid[x][y] = self.def_value
-			end
-		end
-	end
+  --[[ Resets a given x,y cell to the grid default value. ]]
+  function g:reset_cell(x, y)
+    if self:is_valid(x, y) then
+      self._grid[x][y] = self.def_value
+      return true
+    else
+      return false
+    end
+  end
 
-	--[[
-	-- This method is used to populate multiple cells at once.
-	-- The 'data' argument must be a table, with each element
-	-- consisting of three values: x, y, and the data to set
-	-- the cell too. IE:
-	--   d = {{4, 4, "X"}, {4, 5, "O"}, {5, 4, "O"}, {5, 5, "X"}}
-	--   G:populate(d)
-	-- If the object to be populated is nil, it is replaced with
-	-- the default value.
-	--]]
-	function g:populate(data)
-		if type(data) ~= "table" then 
-			return false
-		end
+  --[[ Resets the entire grid to the default value. ]]
+  function g:reset_all()
+    for x=1, self.size_x do
+      for y=1, self.size_y do
+        self._grid[x][y] = self.def_value
+      end
+    end
+  end
 
-		for i, v in data do
-			x, y, obj = unpack(v)
-			if self:is_valid(x, y) then
-				if obj == nil then 
-					obj = self.def_value
-				end
+  --[[
+  -- This method is used to populate multiple cells at once.
+  -- The 'data' argument must be a table, with each element
+  -- consisting of three values: x, y, and the data to set
+  -- the cell too. IE:
+  --   d = {{4, 4, "X"}, {4, 5, "O"}, {5, 4, "O"}, {5, 5, "X"}}
+  --   G:populate(d)
+  -- If the object to be populated is nil, it is replaced with
+  -- the default value.
+  --]]
+  function g:populate(data)
+    if type(data) ~= "table" then return false end
+
+    for i, v in data do
+      x, y, obj = unpack(v)
+
+      if self:is_valid(x, y) then
+        if obj == nil then 
+          obj = self.def_value
+        end
+
 				self._grid[x][y] = obj
-			end
-		end
-		return true
-	end
+      end
+    end
 
-	--[[
-	-- This method returns the entire grid's contents in a
-	-- flat table suitable for feeding to populate() above.
-	-- Useful for recreating a grid layout.
-	-- If the 'no_default' argument is non-nil, then the 
-	-- returned data table only contains elements who's 
-	-- cells are not the default value.
-	--]]
-	function g:get_contents(no_default)
-		local data = {}
-		local x, y
-		local cell_obj = nil
-		for x=1, self.size_x do
-			for y=1, self.size_y do
-				cell_obj = self._grid[x][y]
-				if no_default == true and cell_obj == self.def_value then
-					-- Do nothing, ignore default values.
-				else
-					table.insert(data, {x, y, cell_obj})
-				end
-			end
-		end
-		return data
-	end
+    return true
+  end
 
-	--[[ 
-	-- Convience method to return an x,y vector pair from the
-	-- GRID_* vector constants. Or nil if there is no such
-	-- constant.
-	--]]
-	function g:get_vector(vector)
-		if vector == GRID_TOP_LEFT then
-			return -1, -1
-		elseif vector == GRID_TOP then
-			return -1, 0
-		elseif vector == GRID_TOP_RIGHT then
-			return -1, 1
-		elseif vector == GRID_LEFT then
-			return 0, -1
-		elseif vector == GRID_CENTER then
-			return 0, 0
-		elseif vector == GRID_RIGHT then
-			return 0, 1
-		elseif vector == GRID_BOTTOM_LEFT then
-			return 1, -1
-		elseif vector == GRID_BOTTOM then
-			return 1, 0
-		elseif vector == GRID_BOTTOM_RIGHT then
-			return 1, 1
-		else
-			--[[ Oops. ]]
-			return nil
-		end
-	end
+  --[[
+  -- This method returns the entire grid's contents in a
+  -- flat table suitable for feeding to populate() above.
+  -- Useful for recreating a grid layout.
+  -- If the 'no_default' argument is non-nil, then the
+  -- returned data table only contains elements who's 
+  -- cells are not the default value.
+  --]]
+  function g:get_contents(no_default)
+    local x, y
+    local data     = {}
+    local cell_obj = nil
 
-	--[[ Gets a cell's neighbor in a given vector. ]]
-	function g:get_neighbor(x, y, vector)
-		local obj = nil
-		local vx, vy = self:get_vector(vector)
-		if vx ~= nil then
-			x = x + vx
-			y = y + vy
-			if self:is_valid(x, y) then
-				obj = self:get_cell(x, y)
-			end
-		end
-		return obj		
-	end
+    for x=1, self.size_x do
+      for y=1, self.size_y do
+        cell_obj = self._grid[x][y]
 
-	--[[
-	-- Will return a table of 8 elements, with each element
-	-- representing one of the 8 neighbors for the given
-	-- x,y cell. Each element of the returned table will consist
-	-- of the x,y cell pair, plus the data stored there, suitable
-	-- for use of the populate() method. If the neighbor cell is 
-	-- outside the grid, then {nil, nil, GRID_OUTSIDE} is used for 
-	-- that value.
-	-- If the given x,y values are not sane, an empty table
-	-- is returned instead.
-	--]]
-	function g:get_neighbors(x, y)
-		local data = {}
-		local gx, gy, vx, vy
-		if not self:is_valid(x, y) then
-			return data
-		end
+        if no_default == true and cell_obj == self.def_value then
+          -- Do nothing, ignore default values.
+        else
+          table.insert(data, {x, y, cell_obj})
+        end
+      end
+    end
 
-		--[[
-		-- The vectors used are x,y pairs between -1 and +1
-		-- for the given x,y cell. 
-		-- IE: 
-		--     (-1, -1) (0, -1) (1, -1)
-		--     (-1,  0) (0,  0) (1,  0)
-		--     (-1,  1) (0,  1) (1,  1)
-		-- Value of 0,0 is ignored, since that is the cell
-		-- we are working with! :D
-		--]]
-		for gx = -1, 1 do
-			for gy = -1, 1 do
-				vx = x + gx
-				vy = y + gy
-				if (gx == 0 and gy == 0) then
-					-- Do nothing
-				elseif self:is_valid(vx, vy) then
-					table.insert(data, {vx, vy, self._grid[vx][vy]})
-				else
-					table.insert(data, {nil, nil, GRID_OUTSIDE})
-				end
-			end
-		end
-		return data
-	end
+    return data
+  end
 
-	--[[
-	-- This method will change the grid size. If the new size is
-	-- smaller than the old size, data in the cells now 'outside'
-	-- the grid is lost. If the grid is now larger, new cells are
-	-- filled with the default value given when the grid was first
-	-- created.
-	--]]
-	function g:resize(newx, newy)
-		if (type(newx) ~= "number" or newx == nil) or
-		   (type(newy) ~= "number" or newy == nil) then
-			return false
-		end
+  --[[ 
+  -- Convience method to return an x,y vector pair from the
+  -- GRID_* vector constants. Or nil if there is no such
+  -- constant.
+  --]]
+  function g:get_vector(vector)
+    if vector == GRID_TOP_LEFT then
+      return -1, -1
+    elseif vector == GRID_TOP then
+      return -1, 0
+    elseif vector == GRID_TOP_RIGHT then
+      return -1, 1
+    elseif vector == GRID_LEFT then
+      return 0, -1
+    elseif vector == GRID_CENTER then
+      return 0, 0
+    elseif vector == GRID_RIGHT then
+      return 0, 1
+    elseif vector == GRID_BOTTOM_LEFT then
+      return 1, -1
+    elseif vector == GRID_BOTTOM then
+      return 1, 0
+    elseif vector == GRID_BOTTOM_RIGHT then
+      return 1, 1
+    else
+      --[[ Oops. ]]
+      return nil
+    end
+  end
 
-		local c, x, y
+  --[[ Gets a cell's neighbor in a given vector. ]]
+  function g:get_neighbor(x, y, vector)
+    local obj    = nil
+    local vx, vy = self:get_vector(vector)
 
-		-- Save old data.
-		c = self:get_contents()
+    if vx ~= nil then
+      x = x + vx
+      y = y + vy
 
-		-- Destroy/reset grid.
-		self._grid = {}
+      if self:is_valid(x, y) then
+        obj = self:get_cell(x, y)
+      end
+    end
 
-		for x=1, newx do
-			self._grid[x] = {}
-			for y=1, newy do
-				table.insert(self._grid[x], self.def_value)
-			end
-		end
+    return obj
+  end
 
-		self.size_x = newx
-		self.size_y = newy
+  --[[
+  -- Will return a table of 8 elements, with each element
+  -- representing one of the 8 neighbors for the given
+  -- x,y cell. Each element of the returned table will consist
+  -- of the x,y cell pair, plus the data stored there, suitable
+  -- for use of the populate() method. If the neighbor cell is 
+  -- outside the grid, then {nil, nil, GRID_OUTSIDE} is used for 
+  -- that value.
+  -- If the given x,y values are not sane, an empty table
+  -- is returned instead.
+  --]]
+  function g:get_neighbors(x, y)
+    local data = {}
+    local gx, gy, vx, vy
 
-		self:populate(c)
+    if not self:is_valid(x, y) then return data end
 
-		return true
-	end
+    --[[
+    -- The vectors used are x,y pairs between -1 and +1
+    -- for the given x,y cell. 
+    -- IE: 
+    --     (-1, -1) (0, -1) (1, -1)
+    --     (-1,  0) (0,  0) (1,  0)
+    --     (-1,  1) (0,  1) (1,  1)
+    -- Value of 0,0 is ignored, since that is the cell
+    -- we are working with! :D
+    --]]
+    for gx = -1, 1 do
+      for gy = -1, 1 do
+        vx = x + gx
+        vy = y + gy
 
-	--[[
-	-- This method returns a table of all values in a given 
-	-- row 'x' value.
-	--]]
-	function g:get_row(x)
-		local row = {}
-		if type(x) == "number" and (x > 0 and x <= self.size_x) then
-			row = self._grid[x]
-		end
-		return row
-	end
+        if (gx == 0 and gy == 0) then
+          -- Do nothing
+      	elseif self:is_valid(vx, vy) then
+          table.insert(data, {vx, vy, self._grid[vx][vy]})
+        else
+          table.insert(data, {nil, nil, GRID_OUTSIDE})
+        end
+      end
+    end
 
-	--[[
-	-- This method returns a table of all values in a given
-	-- column 'y' value.
-	--]]
-	function g:get_column(y)
-		local col = {}
-		if type(y) == "number" and (y > 0 and y <= self.size_y) then
-			for x=1, self.size_x do
-				table.insert(col, self._grid[x][y])
-			end
-		end
-		return col
-	end
+    return data
+  end
 
-	--[[
-	-- This method traverses a line of cells, from a given x,y 
-	-- going in 'vector' direction. The vector arg is one of the
-	-- GRID_* traversal constants. This will return a table of 
-	-- data of the cells along the traversal path or nil if 
-	-- the original x,y is not valid or if the vector is not one
-	-- of the constant values.
-	-- In the returned table, each element will be in the format 
-	-- of {x, y, obj}, suitable for populate().
-	--]]
-	function g:traverse(x, y, vector)
-		local data = {}
-		local gx, gy, vx, vy
+  --[[
+  -- This method will change the grid size. If the new size is
+  -- smaller than the old size, data in the cells now 'outside'
+  -- the grid is lost. If the grid is now larger, new cells are
+  -- filled with the default value given when the grid was first
+  -- created.
+  --]]
+  function g:resize(newx, newy)
+    if (type(newx) ~= "number" or newx == nil) or
+       (type(newy) ~= "number" or newy == nil) then
+      return false
+    end
 
-		if self:is_valid(x, y) then
-			vx, vy = self:get_vector(vector)
+    local c, x, y
 
-			if vx == nil then
-				-- table is still empty.
-				return data
-			end
+    -- Save old data.
+    c = self:get_contents()
 
-			gx = x + vx
-			gy = y + vy
+    -- Destroy/reset the internal grid.
+    self._grid = {}
 
-			while self:is_valid(gx, gy) do
-				local obj = self:get_cell(gx, gy)
-				table.insert(data, {gx, gy, obj})
-				gx = gx + vx
-				gy = gy + vy
-			end
+    for x=1, newx do
+      self._grid[x] = {}
 
-			return data
-		else
-			return nil
-		end
-	end
+      for y=1, newy do
+        table.insert(self._grid[x], self.def_value)
+      end
+    end
 
-	--[[ END OF METHODS ]]
+    -- Set the new sizes.
+    self.size_x = newx
+    self.size_y = newy
 
-	--[[ Our object is formed, return to momma... ]]
-	return g
+    -- Restore the contents.
+    self:populate(c)
 
+    return true
+  end
+
+  --[[
+  -- This method returns a table of all values in a given 
+  -- row 'x' value.
+  --]]
+  function g:get_row(x)
+    local row = {}
+
+    if type(x) == "number" and (x > 0 and x <= self.size_x) then
+      row = self._grid[x]
+    end
+
+    return row
+  end
+
+  --[[
+  -- This method returns a table of all values in a given
+  -- column 'y' value.
+  --]]
+  function g:get_column(y)
+    local col = {}
+
+    if type(y) == "number" and (y > 0 and y <= self.size_y) then
+      for x=1, self.size_x do
+        table.insert(col, self._grid[x][y])
+      end
+    end
+
+    return col
+  end
+
+  --[[
+  -- This method traverses a line of cells, from a given x,y 
+  -- going in 'vector' direction. The vector arg is one of the
+  -- GRID_* traversal constants. This will return a table of 
+  -- data of the cells along the traversal path or nil if 
+  -- the original x,y is not valid or if the vector is not one
+  -- of the constant values.
+  -- In the returned table, each element will be in the format 
+  -- of {x, y, obj}, suitable for populate().
+  --]]
+  function g:traverse(x, y, vector)
+    local data = {}
+    local gx, gy, vx, vy
+
+    if self:is_valid(x, y) then
+      vx, vy = self:get_vector(vector)
+
+      if vx == nil then
+        -- table is still empty.
+        return data
+      end
+
+      gx = x + vx
+      gy = y + vy
+
+      while self:is_valid(gx, gy) do
+        local obj = self:get_cell(gx, gy)
+
+        table.insert(data, {gx, gy, obj})
+        
+        gx = gx + vx
+        gy = gy + vy
+      end
+
+      return data
+    end
+
+    return nil
+  end
+
+  --[[ END OF METHODS ]]
+
+  --[[ Our object is formed, return to momma... ]]
+  return g
 end
 
 
